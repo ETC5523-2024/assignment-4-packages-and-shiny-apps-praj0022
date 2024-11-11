6d7f013b50045c2b328b27e11e81036d31f0a88b
Pooja Rajendran Raju <praj0022@student.monash.edu>
2024-11-12 02:17:33 +1100

    Description, roxygen2 doc

---
+++ R/launch_app.R
@@ -0,0 +1,18 @@
+#' Launch Coping Methods Shiny App
+#'
+#'This function launches the Shiny app named copingmethods_app included in the copingmethods package.
+#'
+#' @return No return value. This function is called for its side effects (launching the Shiny app).
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#'   launch_shiny_app()
+#' }
+launch_copingmethods <- function() {
+  app_dir <- system.file("copingmethods_app", package = "copingmethods")
+  if (app_dir == "") {
+    stop("Could not find the Shiny app directory. Try re-installing `copingmethods`.", call. = FALSE)
+  }
+  shiny::runApp(app_dir, display.mode = "normal")
+}
